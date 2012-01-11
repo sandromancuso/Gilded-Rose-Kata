43cffeb3c4dcd7ae094b89ec9800b0c7a5a7ffb0
Sandro Mancuso <sandromancuso@yahoo.com>
2012-01-11 23:49:44 +0000

    Import clean up

--- src/test/java/org/gildedrose/GildedRoseTest.java
+++ src/test/java/org/gildedrose/GildedRoseTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
